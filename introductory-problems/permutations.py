1c9269e31d0e83188fb418cd3e4ce0b7d2280167
aman-rwt <amanrawat4060@gmail.com>
2026-03-07 18:24:52 +0530

    update permutations.py

--- introductory-problems/permutations.py
+++ introductory-problems/permutations.py
@@ -1,23 +1,20 @@
 def beautiful(n: int):
-    arr_even = []
-    arr_odd = []
     if n == 1:
         print(1)
         return
     if n == 2 or n == 3:
         print("NO SOLUTION")
         return
+
+    arr_even = []
+    arr_odd = []
+    
     for i in range(1, n + 1):
         if i % 2 == 0:
             arr_even.append(i)
         else:
             arr_odd.append(i)
-    if abs(arr_even[-1] - arr_odd[0]) > 1:
-        arr_even.extend(arr_odd)
-    else:
-        print("NO SOLUTION")
-        return 
-    for item in arr_even:
-        print(item, end=" ")
+    result = arr_even + arr_odd 
+    print(*result, end="")
 
 beautiful(int(input()))
